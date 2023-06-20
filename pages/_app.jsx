5f09bdd015d480c99bc0c6bf23491cee5bb01b3d
pustart <polaris9955@gmail.com>
2023-06-20 14:23:45 +0300

    feat: add next.js fonts optimization

---
+++ pages/_app.jsx
@@ -0,0 +1,20 @@
+import "../src/styles/global.css";
+import Head from "next/head";
+import React from "react";
+import { roboto } from '../src/fonts/fonts';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <main className={roboto.variable}>
+      <Head>
+        <title>Exchanger — приложение для обмена вещей</title>
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" href="/images/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
+}
+
+export default MyApp;
---
+++ src/fonts/fonts.js
@@ -0,0 +1,8 @@
+
+import { Roboto } from 'next/font/google';
+
+export const roboto = Roboto({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['100', '300', '400', '500', '700'],
+  variable: '--font-roboto',
+});

a1bdf808e8330d0dbece5c8c6b852c21ab7216c7
pustart <polaris9955@gmail.com>
2023-06-20 23:49:33 +0300

    feat: add button component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,21 @@
+import React from 'react';
+import Button from "../src/components/elements/Button/Button"
+
 export default function Home() {
   return (
+    <>
     <h1>
       Привет мир!
     </h1>
+      <Button appearance="outlined">
+      Кликни на меня
+    </Button>
+      <Button appearance="text">
+        Кликни на меня
+      </Button>
+      <Button>
+        Кликни на меня
+      </Button>
+    </>
   );
 }

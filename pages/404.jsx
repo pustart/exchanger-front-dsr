faf3c2ea9a3efcb026c0c1065d0ac3dde55a6603
pustart <polaris9955@gmail.com>
2023-06-26 21:27:07 +0300

    feat: add custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import Button from "../src/components/elements/Button/Button";
+import styles from "../src/styles/404.module.css";
+import withLayout from '@/src/layouts/layout';
+import { useRouter } from 'next/router';
+
+export function Error404() {
+  const router = useRouter();
+
+  const goToHome = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className={styles.container}>
+      <Image
+        src="/images/404-placeholder.png"
+        width={1060}
+        height={940}
+        alt="Error 404."
+        className={styles.img}
+      />
+      <Button appearance="outlined" onClick={() => {
+        goToHome();
+      }}>
+        Вернуться на главную
+      </Button>
+    </div>
+  );
+}
+
+export default withLayout(Error404);

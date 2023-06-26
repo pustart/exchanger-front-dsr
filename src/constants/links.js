779a2bf73e6bbac3f3f9d6fa93c9ae4daf8ceb64
pustart <polaris9955@gmail.com>
2023-06-26 21:24:25 +0300

    feat: add links constants

---
+++ src/constants/links.js
@@ -0,0 +1,7 @@
+export const PRIVACY_POLICY_LINK = "https://github.com/pustart/exchanger-front-dsr/tree/docs/documentation";
+export const SOCIALS = [
+  { path: "https://www.instagram.com/artempust/", img: "/images/instagram-logo.svg", alt: "Instagram link.", },
+  { path: "https://twitter.com/artyompust", img: "/images/twitter-logo.svg", alt: "Twitter link.", },
+  { path: "https://www.youtube.com/", img: "/images/youtube-logo.svg", alt: "YouTube link.", },
+  { path: "https://github.com/pustart", img: "/images/github-logo.svg", alt: "GitHub link.", },
+];

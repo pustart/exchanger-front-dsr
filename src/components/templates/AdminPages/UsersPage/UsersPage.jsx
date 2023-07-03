745efe0a47662bbce104ae2dea77b9e37a300db8
pustart <polaris9955@gmail.com>
2023-07-03 14:48:19 +0300

    feat: add map generation for user cards

--- src/components/templates/AdminPages/UsersPage/UsersPage.jsx
+++ src/components/templates/AdminPages/UsersPage/UsersPage.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import Pagination from "../../../elements/Pagination/Pagination";
 import styles from "./UsersPage.module.css";
-import UserCard from "../../../modules/UserCard/UserCard";
+import UserCard from "../../../elements/UserCard/UserCard";
+import { USERS } from "../../../../constants/mocks";
 
 function UsersPage() {
   return (
     <div className={styles["users-page-container"]}>
       <div className={styles["user-card-container"]}>
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
+        {USERS.map(user => (
+          <UserCard key={user.id} userInfo={user} />
+        ))}
       </div>
       <Pagination count={5} />
     </div>

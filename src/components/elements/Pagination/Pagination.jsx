d8dc64728693d341dbbc555f1de7b1c6a785c691
pustart <polaris9955@gmail.com>
2023-07-03 14:40:49 +0300

    feat: add custom styles for pagination

--- src/components/elements/Pagination/Pagination.jsx
+++ src/components/elements/Pagination/Pagination.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import MuiPagination from "@mui/material/Pagination"; // import styles from "./Link.module.css";
 import { styled } from "@mui/material/styles";
+import PaginationItem from "@mui/material/PaginationItem";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const StyledPagination = styled(MuiPagination)(() => ({
   ul: {
+    marginTop: "2rem",
+    marginBottom: "3.25rem",
     "& .MuiPaginationItem-root": {
       color: "inherit",
-      "&:hover, &.Mui-selected": {
-        color: "var(--accent-strong)",
+      "&.Mui-selected": {
+        color: "var(--faint-weak-down)",
+        backgroundColor: "var(--accent-strong)",
       },
     },
   },
 }));
 
 function Pagination({ className, count, ...props }) {
-  return <StyledPagination count={count} className={className} {...props} />;
+  return (
+    <StyledPagination
+      count={count}
+      className={className}
+      shape="rounded"
+      renderItem={item => (
+        <PaginationItem slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }} {...item} />
+      )}
+      {...props}
+    />
+  );
 }
 
 export default Pagination;

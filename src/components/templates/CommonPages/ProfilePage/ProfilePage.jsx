d9d049327d4798c2f25e135ec71edd54c38b0447
pustart <polaris9955@gmail.com>
2023-07-04 11:31:35 +0300

    feat: add profile page template

--- src/components/templates/CommonPages/ProfilePage/ProfilePage.jsx
+++ src/components/templates/CommonPages/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./ProfilePage.module.css";
+import ProfileMenu from "../../../modules/ProfileMenu/ProfileMenu";
+import ROLES from "../../../../constants/roles";
+import ProfileSettings from "../../../modules/ProfileSettings/ProfileSettings";
 
-function ProfilePage() {
+function ProfilePage({ userRole = ROLES.USER, children }) {
   return (
     <div className={styles["grid-container"]}>
-      <div className={styles.sidebar}>Hello</div>
-      <div className={styles.main}>Main</div>
+      <ProfileMenu userRole={userRole} />
+      <div className={styles.main}>{userRole === ROLES.ADMIN ? <ProfileSettings /> : children}</div>
     </div>
   );
 }

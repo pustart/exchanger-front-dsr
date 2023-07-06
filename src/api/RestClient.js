bbd73ba0cb450c8449ce2e659ed7df671b30deee
pustart <polaris9955@gmail.com>
2023-07-06 11:18:50 +0300

    feat: add axios functions for api endpoints

---
+++ src/api/RestClient.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import axios from "axios";
+
+axios.defaults.headers.common.Accept = 'application/json';
+axios.defaults.timeout = 12000;
+
+const getHttpHeaders = (isAuthenticated = false) => {
+  // Add your custom logic here, for example add a Token to the Headers
+  if (isAuthenticated) {
+    return {
+      headers: {
+        Authorization: 'Bearer YOUR_TOKEN',
+      },
+    };
+  }
+
+  return {};
+};
+
+const get = path => axios.get(path, getHttpHeaders());
+
+const del = path => axios.delete(path, getHttpHeaders());
+
+const post = (path, data) => axios.post(path, data, getHttpHeaders());
+
+const put = (path, data) => axios.post(path, data, getHttpHeaders());
+
+const patch = (path, data) => axios.post(path, data, getHttpHeaders());
+
+export default {
+  get,
+  del,
+  post,
+  put,
+  patch,
+};
---
+++ src/constants/api.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export const BACKEND_PATH = "https://exchanger-backend.onrender.com";

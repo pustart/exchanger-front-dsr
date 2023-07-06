6915e0847b08d3bdf28abfa892590f586167956f
pustart <polaris9955@gmail.com>
2023-07-06 21:45:54 +0300

    feat: add banner component

--- src/components/elements/Banner/Banner.jsx
+++ src/components/elements/Banner/Banner.jsx
@@ -3,13 +3,14 @@ import cn from "classnames";
 import styles from "./Banner.module.css";
 import Htag from "../Htag/Htag";
 
-function Banner({ className, title, ...props }) {
+function Banner({ className, title, children, ...props }) {
   return (
     <aside className={cn(styles.banner, className)} {...props}>
       <div className={styles["content-wrapper"]}>
         <Htag className={styles.title} tag="h2" fontWeight="bold">
           {title}
         </Htag>
+        {children}
       </div>
     </aside>
   );

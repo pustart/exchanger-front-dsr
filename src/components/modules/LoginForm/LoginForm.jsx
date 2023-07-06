b9aa909012ec6e194221641e314400293f19c60e
pustart <polaris9955@gmail.com>
2023-07-06 16:31:03 +0300

    feat: add login form module

---
+++ src/components/modules/LoginForm/LoginForm.jsx
@@ -0,0 +1,80 @@
+import { React, useState } from "react";
+import cn from "classnames";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import styles from "./LoginForm.module.css";
+import Htag from "../../elements/Htag/Htag";
+import Input from "../../elements/Input/Input";
+import Button from "../../elements/Button/Button";
+
+function LoginForm({ className, title, ...props }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = e => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  return (
+    <div className={cn(styles["login-wrapper"], className)} {...props}>
+      <form onSubmit={handleSubmit} className={styles["login-form"]}>
+        <Htag tag="h3" fontWeight="medium" className={styles["form-title"]}>
+          Авторизируйтесь
+        </Htag>
+        <p className={cn(styles.paragraph, styles["form-description"])}>
+          Пожалуйста, войдите, используя реквизиты учетной записи.
+        </p>
+        <div className={styles["input-wrapper"]}>
+          <Input
+            value={email}
+            placeholder="Введите ваш e-mail"
+            type="email"
+            name="email"
+            id="email"
+            required
+            onChange={handleChangeEmail}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles["input-wrapper"]}>
+          <Input
+            value={password}
+            placeholder="Введите ваш пароль"
+            type="password"
+            name="password"
+            id="password"
+            required
+            onChange={handleChangePassword}
+            className={styles.input}
+          />
+        </div>
+        <Button
+          round="squared"
+          appearance="contained"
+          height="3rem"
+          type="submit"
+          className={styles["login-btn"]}
+        >
+          Авторизироваться
+        </Button>
+        <p className={cn(styles.paragraph, styles["link-wrapper"])}>
+          <NextLink href="/" className={styles.link}>
+            Еще нет аккаунта? Зарегестрируйтесь
+          </NextLink>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;

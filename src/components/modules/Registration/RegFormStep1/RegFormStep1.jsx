eba72421de8a327925409d8d2d4723d19bb19ec7
pustart <polaris9955@gmail.com>
2023-07-06 21:47:35 +0300

    feat: add sign up step 1

---
+++ src/components/modules/Registration/RegFormStep1/RegFormStep1.jsx
@@ -0,0 +1,109 @@
+import { React, useState } from "react";
+import cn from "classnames";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import styles from "./RegFormStep1.module.css";
+import Htag from "../../../elements/Htag/Htag";
+import Input from "../../../elements/Input/Input";
+import Button from "../../../elements/Button/Button";
+
+function RegFormStep1({ className, title, ...props }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const router = useRouter();
+
+  const handleChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = e => {
+    setPassword(e.target.value);
+  };
+
+  const handlePasswordConfirm = e => {
+    setPasswordConfirm(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    router.push("/registration/step2");
+  };
+
+  return (
+    <div className={cn(styles["login-wrapper"], className)} {...props}>
+      <form onSubmit={handleSubmit} className={styles["login-form"]}>
+        <Htag tag="h3" fontWeight="medium" className={styles["form-title"]}>
+          Шаг первый
+        </Htag>
+        <p className={cn(styles.paragraph, styles["form-description"])}>
+          Пожалуйста, заполните данную форму для регистрации
+        </p>
+        <div className={styles["input-wrapper"]}>
+          <Input
+            value={email}
+            placeholder="Введите ваш e-mail"
+            type="email"
+            name="email"
+            id="email"
+            required
+            onChange={handleChangeEmail}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles["input-wrapper"]}>
+          <Input
+            value={password}
+            placeholder="Введите ваш пароль"
+            type="password"
+            name="password"
+            id="password"
+            required
+            onChange={handleChangePassword}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles["input-wrapper"]}>
+          <Input
+            value={passwordConfirm}
+            placeholder="Подтвердите ваш пароль"
+            type="password"
+            name="passwordConfirm"
+            id="passwordConfirm"
+            required
+            onChange={handlePasswordConfirm}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles["btn-container"]}>
+          <Button
+            round="squared"
+            appearance="outlined"
+            height="3rem"
+            type="button"
+            className={styles["back-btn"]}
+            onClick={() => router.back()}
+          >
+            Назад
+          </Button>
+          <Button
+            round="squared"
+            appearance="contained"
+            height="3rem"
+            type="submit"
+            className={styles["login-btn"]}
+          >
+            Далее
+          </Button>
+        </div>
+        <p className={cn(styles.paragraph, styles["link-wrapper"])}>
+          <NextLink href="/login" className={styles.link}>
+            Уже есть аккаунт? Войдите
+          </NextLink>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default RegFormStep1;

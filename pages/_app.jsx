e31a41adcecb0106b127a80c66eb8aadc9bee3cf
pustart <polaris9955@gmail.com>
2023-07-08 16:42:17 +0300

    feat: add next auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,22 +3,25 @@ import "../src/styles/global.css";
 import Head from "next/head";
 import React from "react";
 import { StyledEngineProvider } from "@mui/material/styles";
+import { SessionProvider } from "next-auth/react";
 import { roboto } from "../src/fonts/fonts";
 import { wrapper } from "../src/store/store";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <StyledEngineProvider injectFirst>
-      <main className={roboto.variable}>
-        <Head>
-          <title>Exchanger — приложение для обмена вещей</title>
-          <link rel="icon" href="/favicons/favicon.ico" />
-          <link rel="apple-touch-icon" href="/images/favicons/apple-touch-icon.png" />
-          <link rel="manifest" href="/manifest.webmanifest" />
-        </Head>
-        <Component {...pageProps} />
-      </main>
-    </StyledEngineProvider>
+    <SessionProvider session={session}>
+      <StyledEngineProvider injectFirst>
+        <main className={roboto.variable}>
+          <Head>
+            <title>Exchanger — приложение для обмена вещей</title>
+            <link rel="icon" href="/favicons/favicon.ico" />
+            <link rel="apple-touch-icon" href="/images/favicons/apple-touch-icon.png" />
+            <link rel="manifest" href="/manifest.webmanifest" />
+          </Head>
+          <Component {...pageProps} />
+        </main>
+      </StyledEngineProvider>
+    </SessionProvider>
   );
 }
 

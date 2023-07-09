555305f37e7a496dc596cc12612466c983c20aea
pustart <polaris9955@gmail.com>
2023-07-09 20:00:02 +0300

    feat: add redirect to error next-auth page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,25 +18,30 @@ export default NextAuth({
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
+        try {
+          const res = await fetch("https://exchanger-backend.onrender.com/auth/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+          });
 
-        const res = await fetch("https://exchanger-backend.onrender.com/auth/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: credentials?.email,
-            password: credentials?.password,
-          }),
-        });
-        const user = await res.json();
+          const user = await res.json();
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
+          if (res.ok) {
+            // Any object returned will be saved in `user` property of the JWT
+            return user;
+          }
+
+          throw new Error(res.statusText);
+        } catch (error) {
+          throw new Error('Next Auth - Authorize: Authentication error');
         }
         // If you return null then an error will be displayed advising the user to check their details.
-        return null;
         // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
       },
     }),

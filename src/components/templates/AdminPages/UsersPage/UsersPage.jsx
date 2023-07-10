697be0eae21a1612fecdf47c4ea55cdbee1a51a5
pustart <polaris9955@gmail.com>
2023-07-10 12:20:40 +0300

    feat: add placeholder text for empty pages

--- src/components/templates/AdminPages/UsersPage/UsersPage.jsx
+++ src/components/templates/AdminPages/UsersPage/UsersPage.jsx
@@ -4,19 +4,26 @@ import Pagination from "../../../elements/Pagination/Pagination";
 import styles from "./UsersPage.module.css";
 import UserCard from "../../../elements/UserCard/UserCard";
 import { USERS } from "../../../../constants/mocks";
+import Htag from "../../../elements/Htag/Htag";
 
 function UsersPage({ users = USERS, pagination = true }) {
   const [animationParent] = useAutoAnimate();
 
   return (
     <div className={styles["users-page-container"]}>
-      <ul ref={animationParent} className={styles["user-card-container"]}>
-        {users.map(user => (
-          <li key={user.id}>
-            <UserCard key={user.id} userInfo={user} />
-          </li>
-        ))}
-      </ul>
+      {users.length === 0 ? (
+        <Htag tag="h3" fontWeight="medium" className={styles.placeholder}>
+          Пользователи не найдены
+        </Htag>
+      ) : (
+        <ul ref={animationParent} className={styles["user-card-container"]}>
+          {users.map(user => (
+            <li key={user.id}>
+              <UserCard key={user.id} userInfo={user} />
+            </li>
+          ))}
+        </ul>
+      )}
       {pagination ? <Pagination count={5} /> : null}
     </div>
   );
